77eb1eedb4366a79011e38b9c5cb892245903921
Sailaja <rsailaja@hotmail.com>
2018-02-27 15:31:01 +1100

    Initial commit

---
+++ LocalTopics/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Event.swift
+//  LocalTopics
+//
+//  Created by Sailaja Rallapalli on 27/02/18.
+//  Copyright © 2018 SoftSol. All rights reserved.
+//
+
+import Foundation
+
+struct Event : Decodable{
+    var name: String?
+    var desc: String?
+    var imageUrl: String?
+}
