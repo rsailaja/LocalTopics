99f32c090dfd6e55263e4f85d27b4e8e88a20b75
rsailaja <rsailaja@hotmail.com>
2018-02-27 15:20:29 +1100

    Initial Commit

---
+++ LocalTopics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalTopics
+//
+//  Created by Sailaja Rallapalli on 27/02/18.
+//  Copyright © 2018 SoftSol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
